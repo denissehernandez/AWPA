f510a270c6a17a7cd0608147f9980fc13a8cb4b3
DENISSE <hernandezdenisse504@gmail.com>
2022-12-05 20:13:27 -0600

    a

--- sw.js
+++ sw.js
@@ -25,5 +25,21 @@ self.addEventListener('install', e=>{
 
 self.addEventListener('fetch', e =>{
     //cache-only
-    e.respondWith(caches.match(e.request));
+    const respuesta = caches.match( e.request )
+    .then ( res => {
+        if ( res ) return res;
+        //no existe el archivo
+        //tengo que ir a la web
+        console.log('No existe', e.request.url);
+        return fetch( e.request ).then ( newResp => {
+            caches.open('cache-v1')
+                .then( cache => {
+                    cache.put( e.request, newResp);
+                }
+
+                )
+            return newResp.clone;
+        });
+    });
+    e.respondWith(respuesta);
 })
